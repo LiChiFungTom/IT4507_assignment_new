5b35d6f2ecbe88458375b2772fe18b9640840332
LiChiFungTom <72788072+LiChiFungTom@users.noreply.github.com>
2021-11-05 12:38:35 +0800

    Update RemoteControlCarFactory.java

--- assignment_new/Assignment/RemoteControlCarFactory.java
+++ assignment_new/Assignment/RemoteControlCarFactory.java
@@ -7,8 +7,8 @@ public class RemoteControlCarFactory implements ToyProductFactory{
     public ToyProduct createProduct(Scanner sc){
             System.out.println("Enter id");
             int id = sc.nextInt();
-            System.out.println("Enter name");
-            String name = sc.next();
+            System.out.println("Enter name"+sc.nextLine());
+            String name = sc.nextLine();
             System.out.println("maximum speed(km/hr)");
             double speed = sc.nextDouble();
             System.out.println(id+","+name+","+speed);
